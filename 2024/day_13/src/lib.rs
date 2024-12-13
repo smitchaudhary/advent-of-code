88341874448979a97d1de73505bd3c69dcca9449
Smit Chaudhary <smitchaudhary10@gmail.com>
2024-12-13 13:01:10 +0530

    Add day 13 part 2

--- 2024/day_13/src/lib.rs
+++ 2024/day_13/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod solutions {
     use std::fs::read_to_string;
 
-    pub fn get_tokens(filename: &String) -> i32 {
+    pub fn get_tokens(filename: &String) -> i64 {
         let contents = read_to_string(filename).unwrap();
 
         let mut tokens = 0;
@@ -15,7 +15,21 @@ pub mod solutions {
         tokens
     }
 
-    fn get_machine_stats(input: &str) -> ((i32, i32), (i32, i32), (i32, i32)) {
+    pub fn get_tokens_converted(filename: &String) -> i64 {
+        let contents = read_to_string(filename).unwrap();
+
+        let mut tokens = 0;
+
+        for game in contents.split("\n\n") {
+            let (button_a, button_b, target) = get_machine_stats(game);
+            let target = (target.0 + 10000000000000, target.1 + 10000000000000);
+            tokens += get_cost(button_a, button_b, target);
+        }
+
+        tokens
+    }
+
+    fn get_machine_stats(input: &str) -> ((i64, i64), (i64, i64), (i64, i64)) {
         let mut lines = input.lines();
         let button_a = lines
             .next()
@@ -24,8 +38,8 @@ pub mod solutions {
             .nth(1)
             .unwrap()
             .split(",")
-            .map(|cap| cap.split("+").nth(1).unwrap().parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+            .map(|cap| cap.split("+").nth(1).unwrap().parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         let button_b = lines
             .next()
@@ -34,8 +48,8 @@ pub mod solutions {
             .nth(1)
             .unwrap()
             .split(",")
-            .map(|cap| cap.split("+").nth(1).unwrap().parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+            .map(|cap| cap.split("+").nth(1).unwrap().parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         let prize = lines
             .next()
@@ -44,8 +58,8 @@ pub mod solutions {
             .nth(1)
             .unwrap()
             .split(",")
-            .map(|cap| cap.split("=").nth(1).unwrap().parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+            .map(|cap| cap.split("=").nth(1).unwrap().parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         (
             (button_a[0], button_a[1]),
@@ -54,7 +68,7 @@ pub mod solutions {
         )
     }
 
-    fn get_cost(button_a: (i32, i32), button_b: (i32, i32), prize: (i32, i32)) -> i32 {
+    fn get_cost(button_a: (i64, i64), button_b: (i64, i64), prize: (i64, i64)) -> i64 {
         let det = button_a.0 * button_b.1 - button_a.1 * button_b.0;
 
         let a = (button_b.1 * prize.0 - button_b.0 * prize.1) / det;
--- 2024/day_13/src/main.rs
+++ 2024/day_13/src/main.rs
@@ -1,4 +1,4 @@
-use day_13::solutions::get_tokens;
+use day_13::solutions::{get_tokens, get_tokens_converted};
 
 fn main() {
     let filename = "input.txt".to_string();
@@ -6,4 +6,8 @@ fn main() {
     let tokens = get_tokens(&filename);
 
     println!("Total {tokens} spent");
+
+    let corrected_tokens = get_tokens_converted(&filename);
+
+    println!("Total tokens in second part: {corrected_tokens}");
 }
