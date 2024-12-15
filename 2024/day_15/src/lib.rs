59e288b4bd88dd5aa98978d54a7e841eb31ae06c
Smit Chaudhary <smitchaudhary10@gmail.com>
2024-12-15 18:17:45 +0530

    Add day 15 part 2

--- 2024/day_15/src/lib.rs
+++ 2024/day_15/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod solutions {
-    use std::fs::read_to_string;
+    use std::{collections::HashSet, fs::read_to_string};
 
     pub fn get_gps_coords(filename: &String) -> u32 {
         let content = read_to_string(filename).unwrap();
@@ -18,6 +18,30 @@ pub mod solutions {
 
         for dirn in instructions.chars() {
             make_move(&mut map, dirn, &mut bot_pos);
+        }
+
+        count_gps(&map)
+    }
+
+    pub fn get_widened_gps_coords(filename: &String) -> u32 {
+        let content = read_to_string(filename).unwrap();
+
+        let map = content
+            .split("\n\n")
+            .nth(0)
+            .unwrap()
+            .lines()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+
+        let mut map = widen(map);
+
+        let instructions = content.split("\n\n").nth(1).unwrap().replace("\n", "");
+
+        let mut bot_pos = initial_pos(&map);
+
+        for dirn in instructions.chars() {
+            make_move_widened(&mut map, dirn, &mut bot_pos);
         }
 
         count_gps(&map)
@@ -62,6 +86,64 @@ pub mod solutions {
         }
     }
 
+    fn make_move_widened(map: &mut Vec<Vec<char>>, dirn: char, bot_pos: &mut (usize, usize)) {
+        let dx: (i32, i32) = match dirn {
+            '^' => (-1, 0),
+            'v' => (1, 0),
+            '<' => (0, -1),
+            '>' => (0, 1),
+            _ => panic!("Invalid character"),
+        };
+
+        let next_pos = (
+            (bot_pos.0 as i32 + dx.0) as usize,
+            (bot_pos.1 as i32 + dx.1) as usize,
+        );
+
+        match map[next_pos.0][next_pos.1] {
+            '.' => {
+                map[bot_pos.0][bot_pos.1] = '.';
+                map[next_pos.0][next_pos.1] = '@';
+                *bot_pos = next_pos;
+            }
+            '[' | ']' => {
+                let mut checked: HashSet<(usize, usize)> = HashSet::new();
+
+                if !check_movable(map, *bot_pos, dx, &mut checked) {
+                    return;
+                }
+
+                while !checked.is_empty() {
+                    let mut to_remove = Vec::new();
+
+                    for &pos in &checked {
+                        let next_pos = (
+                            (pos.0 as i32 + dx.0) as usize,
+                            (pos.1 as i32 + dx.1) as usize,
+                        );
+
+                        if !checked.contains(&next_pos) {
+                            if map[next_pos.0][next_pos.1] != '@' && map[pos.0][pos.1] != '@' {
+                                map[next_pos.0][next_pos.1] = map[pos.0][pos.1];
+                                map[pos.0][pos.1] = '.';
+                            }
+                            to_remove.push(pos);
+                        }
+                    }
+
+                    for pos in to_remove {
+                        checked.remove(&pos);
+                    }
+                }
+
+                map[next_pos.0][next_pos.1] = '@';
+                map[bot_pos.0][bot_pos.1] = '.';
+                *bot_pos = next_pos;
+            }
+            _ => {}
+        }
+    }
+
     fn initial_pos(map: &Vec<Vec<char>>) -> (usize, usize) {
         let row_idx = map.iter().position(|line| line.contains(&'@')).unwrap();
 
@@ -74,11 +156,70 @@ pub mod solutions {
         let mut gps = 0;
         for (row_idx, line) in map.iter().enumerate() {
             for (col_idx, &ch) in line.iter().enumerate() {
-                if ch == 'O' {
+                if ch == 'O' || ch == '[' {
                     gps += row_idx * 100 + col_idx;
                 }
             }
         }
         gps as u32
+    }
+
+    fn check_movable(
+        map: &Vec<Vec<char>>,
+        pos: (usize, usize),
+        dx: (i32, i32),
+        seen: &mut HashSet<(usize, usize)>,
+    ) -> bool {
+        if !seen.insert(pos) {
+            return true;
+        }
+
+        let next_pos = (
+            (pos.0 as i32 + dx.0) as usize,
+            (pos.1 as i32 + dx.1) as usize,
+        );
+
+        match map[next_pos.0][next_pos.1] {
+            '#' => false,
+            '[' => {
+                check_movable(map, next_pos, dx, seen)
+                    && check_movable(map, (next_pos.0, next_pos.1 + 1), dx, seen)
+            }
+            ']' => {
+                check_movable(map, next_pos, dx, seen)
+                    && check_movable(map, (next_pos.0, next_pos.1 - 1), dx, seen)
+            }
+            _ => true,
+        }
+    }
+
+    fn widen(map: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut widened_map: Vec<Vec<char>> = Vec::new();
+        for row in map.iter() {
+            let mut new_row: Vec<char> = Vec::new();
+            for &cell in row {
+                match cell {
+                    '#' => {
+                        new_row.push('#');
+                        new_row.push('#');
+                    }
+                    '.' => {
+                        new_row.push('.');
+                        new_row.push('.');
+                    }
+                    'O' => {
+                        new_row.push('[');
+                        new_row.push(']');
+                    }
+                    '@' => {
+                        new_row.push('@');
+                        new_row.push('.');
+                    }
+                    _ => panic!("Invalid cell character"),
+                }
+            }
+            widened_map.push(new_row);
+        }
+        widened_map
     }
 }
--- 2024/day_15/src/main.rs
+++ 2024/day_15/src/main.rs
@@ -1,4 +1,4 @@
-use day_15::solutions::get_gps_coords;
+use day_15::solutions::{get_gps_coords, get_widened_gps_coords};
 
 fn main() {
     let filename = "input.txt".to_string();
@@ -6,4 +6,8 @@ fn main() {
     let gps_coords = get_gps_coords(&filename);
 
     println!("GPS Coords: {gps_coords}");
+
+    let widened_gps_coords = get_widened_gps_coords(&filename);
+
+    println!("GPS coords in the widened map: {widened_gps_coords}");
 }
